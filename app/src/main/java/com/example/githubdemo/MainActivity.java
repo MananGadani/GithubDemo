9d6a196754621fd0d248d50802b090747c88697e
harshitdholakia7 <62429097+harshitdholakia7@users.noreply.github.com>
2020-03-20 18:31:24 +0530

    Added Log 1

--- app/src/main/java/com/example/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("*** Debug - ","Manan Gadani"+"ABC");
+        Log.d("*** Debug - ","Manan Gadani"+"ABC");
     }
 }
